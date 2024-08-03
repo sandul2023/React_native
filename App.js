7576b89926fb362a7efa01180705b5a5424155a6
Tharin Sandul <124861676+sandul2023@users.noreply.github.com>
2024-08-03 16:49:12 +0530

    updated

--- App.js
+++ App.js
@@ -18,8 +18,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-//https://lmbmtfbkeupxjhqloudk.supabase.co
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtYm10ZmJrZXVweGpocWxvdWRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5OTg3ODgsImV4cCI6MjAxNjU3NDc4OH0.9_ygHp-b8cPeeJ4r9h_AeIF1Cs6JxttYXtzQKd4hWt0
